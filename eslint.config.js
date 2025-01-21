c52cab60aadfcd0b7714c798e09f7af2e2b578ac
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-01-21 14:22:27 +0530

    test(husky): pre-commit check

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-
 export default [
   { ignores: ['dist'] },
   {
@@ -29,7 +28,10 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
