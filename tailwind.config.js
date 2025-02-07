8fcddefc0b04c950ec0ef5b174d940164a2c8742
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-02-07 18:34:50 +0530

    chore(font): add dm sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#1BAC5F',
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
