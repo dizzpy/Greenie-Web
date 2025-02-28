a46baea8159790f9c204f29051d1835807e504d3
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-02-28 12:32:04 +0530

    feat(auth): created auth pages

---
+++ src/auth/ForgotPassword.jsx
@@ -0,0 +1,10 @@
+const ForgotPassword = () => {
+  return (
+    <div className="forgot-password-container">
+      <h2>Forgot Password</h2>
+      {/* Add your forgot password form here */}
+    </div>
+  );
+};
+
+export default ForgotPassword;
---
+++ src/auth/SetNewPassword.jsx
@@ -0,0 +1,10 @@
+const SetNewPassword = () => {
+  return (
+    <div className="set-new-password-container">
+      <h2>Set New Password</h2>
+      {/* Add your set new password form here */}
+    </div>
+  );
+};
+
+export default SetNewPassword;
