38e905770b430974f20dfb984c0545ca1f721c11
Anuja Rathnayaka <anujanisal19@gmail.com>
2025-03-04 18:03:39 +0530

    fix(leaderboard): 3 cards responsive

--- src/[features]/leaderboard/pages/LeaderboardHome.jsx
+++ src/[features]/leaderboard/pages/LeaderboardHome.jsx
@@ -2,18 +2,18 @@ import TopLeaderboardCard from '../components/TopLeaderboardCard';
 
 function LeaderboardHome() {
   return (
-    <div className="max-w-screen-2xl container mx-auto px-4">
+    <div className="container mx-auto px-4">
       {/* Global nav */}
       <div className="bg-green-300 py-4 mx-auto my-4 rounded-lg">
         <p className="text-center">Global Nav</p>
       </div>
 
-      {/* Main grid container */}
-      <div className="flex gap-4 mt-10">
-        {/* Left section (9/12) */}
-        <div className="w-9/12">
-          {/* 3x3 grid - added padding */}
-          <div className="grid grid-cols-3 gap-4 p-5">
+      {/* Main grid container - make it column on mobile */}
+      <div className="flex flex-col lg:flex-row gap-4 mt-10">
+        {/* Left section - full width on mobile */}
+        <div className="w-full lg:w-9/12">
+          {/* 3x3 grid - 1 column on mobile, 2 on tablet, 3 on desktop */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
             <TopLeaderboardCard
               fullName="Anuja Rathnayaka"
               username="dizzpy"
@@ -35,8 +35,10 @@ function LeaderboardHome() {
           </div>
         </div>
 
-        {/* Right section (3/12) */}
-        <div className="w-3/12 bg-pink-200 p-4 rounded-lg">Right Section</div>
+        {/* Right section - full width on mobile */}
+        <div className="w-full lg:w-3/12 bg-pink-200 p-4 rounded-lg">
+          Right Section
+        </div>
       </div>
     </div>
   );
